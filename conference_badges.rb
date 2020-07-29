2566e46e45137c3b2d944b8b0d92bbbed71d64c1
Nerly Ton <nerlyton@gmail.com>
2020-07-29 22:14:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,12 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
-attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def batch_badge_creator(attendees)
  messages = []
- attendees.each do |array|
+ attendees.each do |names|
    messages << badge_maker(name)
  end
  return messages
