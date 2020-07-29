cf0320a24937b9bfa6586710e52ed375de368cb6
Nerly Ton <nerlyton@gmail.com>
2020-07-29 23:48:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
- assign_rooms(attendees)
+  badge = batch_badge_creator(attendees)
+ rooms = assign_rooms(attendees)
 end
